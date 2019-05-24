9a84f973a6e439a162626938436535295b98e23e
Taylor Huang <tahuan@microsoft.com>
2019-05-24 14:48:12 +0800

    added node chain

---
+++ Pluralsight/SimpNodeChain/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpNodeChain
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node first = new Node { Value = 3 };
+            Node middle = new Node { Value = 5 };
+            first.Next = middle;
+            Node last = new Node { Value = 7 };
+            middle.Next = last;
+
+            // now iterate over each node and print the value
+            PrintList(first);
+        }
+
+        private static void PrintList(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}
